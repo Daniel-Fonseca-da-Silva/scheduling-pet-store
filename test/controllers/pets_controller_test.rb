8d465eade7a9cc00618079bb82f09cee8685088c
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-11-12 18:44:00 -0300

    feature: Created structure to api pet store

---
+++ test/controllers/pets_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class PetsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @pet = pets(:one)
+  end
+
+  test "should get index" do
+    get pets_url, as: :json
+    assert_response :success
+  end
+
+  test "should create pet" do
+    assert_difference("Pet.count") do
+      post pets_url, params: { pet: { name: @pet.name } }, as: :json
+    end
+
+    assert_response :created
+  end
+
+  test "should show pet" do
+    get pet_url(@pet), as: :json
+    assert_response :success
+  end
+
+  test "should update pet" do
+    patch pet_url(@pet), params: { pet: { name: @pet.name } }, as: :json
+    assert_response :success
+  end
+
+  test "should destroy pet" do
+    assert_difference("Pet.count", -1) do
+      delete pet_url(@pet), as: :json
+    end
+
+    assert_response :no_content
+  end
+end
