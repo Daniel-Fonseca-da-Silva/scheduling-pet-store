c6b7b2321617fa18e5b43625e828fe40775dbd0b
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-11-13 00:15:02 -0300

    feature: Created and populate the pet table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# frozen_string_literal: true
+
+Pet.delete_all
+
+20.times do
+  Pet.create name: Faker::Creature::Animal.name
+end
