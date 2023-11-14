97c360152f430064748a4b292e3614ac8e3c0d9e
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-11-14 01:41:01 -0300

    feature: Implemented security with token

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,6 @@
 class Pet < ApplicationRecord
-  validates :name, presence: true
-  scope :search, -> (term) { where('LOWER(name) LIKE ?', "%#{term.downcase}%") if term.present? }
+  validates :name, :token, presence: true
+  scope :by_token, ->(token) { where(token: token) }
+  scope :search, ->(term) { where('LOWER(name) LIKE ?', "%#{term.downcase}%") if term.present? }
   scope :sorted_by_name, -> { order(name: :asc) }
 end
