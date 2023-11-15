3e09dd0cabe0d69770a270fd2cbd797750ba8318
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-11-15 18:29:03 -0300

    feature: Added test to model pet

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,7 @@
 class Pet < ApplicationRecord
-  validates :name, :token, presence: true
+  validates :name, presence: true, uniqueness: { scope: :token, case_sensitive: false }
+  validates :token, presence: true, length: {minimum: 10 }
+
   scope :by_token, ->(token) { where(token: token) }
   scope :search, ->(term) { where('LOWER(name) LIKE ?', "%#{term.downcase}%") if term.present? }
   scope :sorted_by_name, -> { order(name: :asc) }
